625bfa32d0b9c9b80906e600f3a31ca46d2666ac
Nir_Va <64968510+NirVa-gh@users.noreply.github.com>
2024-10-02 13:41:46 +0500

    Условный оператор

--- List.py
+++ List.py
@@ -1,4 +1,5 @@
-list_1 = ["Matvey","Ilya","Miron","Darya"]
+#------------------Списки------------------------------
+"""list_1 = ["Matvey","Ilya","Miron","Darya"]
 list_2 = ["Leonid","Fedor","Makar","Leonid"]
 
 list_1.append("test") #Добавление в список
@@ -10,12 +11,25 @@ print(f"#Удаление, но для всех {list_2}")
 list_1.insert(1,"insert") # Добавление объекта в определенный индекс
 print(f"Добавление объекта в определенный индекс{list_1},{list[2]}")
 a = list_2.pop(2)
-print(a)
-print("test")
+print(a)"""
+#------------------Условный оператор----------------------------
 
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+    if car == 'bmw':  #если car равно bmw то
+        print(car.upper()) # Выполняется в if блоке
+        print(car.upper()) # Выполняется в if блоке
+    else:              #иначе
+        print(car.title()) # будет работать для esle блока
+    print(car.title()) # Будет работать для for блока
+print(car.title())  # Будет работать для основной программы
 
+answer = 17
+if answer != 42:
+    print("That is not the correct answer. Please try again!")
+else:
+    print("That is correct answer")
 
-
-
-
-
+alien_color = "green"
+if alien_color == "green":
+    print("u got a 5$")
